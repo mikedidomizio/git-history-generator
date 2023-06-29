b4d8890fae7c7c0a04d921fff299d08bdbeb9f75
Mike DiDomizio <mikedidomizio@gmail.com>
2023-06-29 08:52:14 -0400

    Can use string for single arrow

--- src/components/Commit.tsx
+++ src/components/Commit.tsx
@@ -1,4 +1,4 @@
-import {ItemProps, ItemWrapper} from "@/components/Item";
+import {ItemProps, ItemWrapper, MakeArrowSafe} from "@/components/Item";
 import clsx from "clsx";
 import {ReactNode} from "react";
 import {Arrow} from "@/components/Arrow";
@@ -26,7 +26,7 @@ export const Commit = (props: CommitProps) => {
         <ItemWrapper id={props.id} classNames={clsx("rounded-2xl", props?.dimmed ? `bg-[#efefe780] text-[#786F6880]` : 'bg-[#efefe7] text-[#786F68]', props?.bounce ? 'animate-bounce' : null)}>
             {props.text}
         </ItemWrapper>
-        {props.arrowTo?.map((arrow) => {
+        {MakeArrowSafe(props.arrowTo).map((arrow) => {
             return <Arrow color={props?.dimmed ? '#786F6880' : '#786F68'} key={arrow.to} arrow={arrow.to} dashedArrow={arrow.dashed || false} id={props.id} />
         })}
     </>
